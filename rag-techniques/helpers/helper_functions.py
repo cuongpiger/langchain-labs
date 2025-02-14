5114124deb6f8cd4bad89cf098fdad55699980ea
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-02-14 06:29:44 +0000

    [chore] nothing to commit

---
+++ rag-techniques/helpers/helper_functions.py
@@ -0,0 +1,158 @@
+from enum import Enum
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+from langchain import PromptTemplate
+
+
+
+# Enum class representing different embedding providers
+class EmbeddingProvider(Enum):
+    OPENAI = "openai"
+    COHERE = "cohere"
+    AMAZON_BEDROCK = "bedrock"
+    VERTEXAI = "vertexai"
+    HUGGINGFACE = "huggingface"
+    
+    
+class QuestionAnswerFromContext(BaseModel):
+    """
+    Model to generate an answer to a query based on a given context.
+    
+    Attributes:
+        answer_based_on_content (str): The generated answer based on the context.
+    """
+    answer_based_on_content: str = Field(description="Generates an answer to a query based on a given context.")
+    
+
+def replace_t_with_space(list_of_documents):
+    """
+    Replaces all tab characters ('\t') with spaces in the page content of each document
+
+    Args:
+        list_of_documents: A list of document objects, each with a 'page_content' attribute.
+
+    Returns:
+        The modified list of documents with tab characters replaced by spaces.
+    """
+
+    for doc in list_of_documents:
+        doc.page_content = doc.page_content.replace('\t', ' ')  # Replace tabs with spaces
+    return list_of_documents
+
+def get_langchain_embedding_provider(provider: EmbeddingProvider, model_id: str = None):
+    """
+    Returns an embedding provider based on the specified provider and model ID.
+
+    Args:
+        provider (EmbeddingProvider): The embedding provider to use.
+        model_id (str): Optional -  The specific embeddings model ID to use .
+
+    Returns:
+        A LangChain embedding provider instance.
+
+    Raises:
+        ValueError: If the specified provider is not supported.
+    """
+    if provider == EmbeddingProvider.OPENAI:
+        from langchain_openai import OpenAIEmbeddings
+        return OpenAIEmbeddings()
+    elif provider == EmbeddingProvider.COHERE:
+        from langchain_cohere import CohereEmbeddings
+        return CohereEmbeddings()
+    elif provider == EmbeddingProvider.AMAZON_BEDROCK:
+        from langchain_community.embeddings import BedrockEmbeddings
+        return BedrockEmbeddings(model_id=model_id) if model_id else BedrockEmbeddings(model_id="amazon.titan-embed-text-v2:0")
+    elif provider == EmbeddingProvider.HUGGINGFACE:
+        from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+        return HuggingFaceEmbeddings(
+            model_name=model_id,
+        ) if model_id else HuggingFaceEmbeddings(
+            model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
+        )
+    else:
+        raise ValueError(f"Unsupported embedding provider: {provider}")
+
+
+def retrieve_context_per_question(question, chunks_query_retriever):
+    """
+    Retrieves relevant context and unique URLs for a given question using the chunks query retriever.
+
+    Args:
+        question: The question for which to retrieve context and URLs.
+
+    Returns:
+        A tuple containing:
+        - A string with the concatenated content of relevant documents.
+        - A list of unique URLs from the metadata of the relevant documents.
+    """
+
+    # Retrieve relevant documents for the given question
+    docs = chunks_query_retriever.get_relevant_documents(question)
+
+    # Concatenate document content
+    # context = " ".join(doc.page_content for doc in docs)
+    context = [doc.page_content for doc in docs]
+
+    return context
+
+
+
+def show_context(context):
+    """
+    Display the contents of the provided context list.
+
+    Args:
+        context (list): A list of context items to be displayed.
+
+    Prints each context item in the list with a heading indicating its position.
+    """
+    for i, c in enumerate(context):
+        print(f"Context {i + 1}:")
+        print(c)
+        print("\n")
+        
+        
+def create_question_answer_from_context_chain(llm):
+    # Initialize the ChatOpenAI model with specific parameters
+    question_answer_from_context_llm = llm
+
+    # Define the prompt template for chain-of-thought reasoning
+    question_answer_prompt_template = """ 
+    For the question below, provide a concise but suffice answer based ONLY on the provided context:
+    {context}
+    Question
+    {question}
+    """
+
+    # Create a PromptTemplate object with the specified template and input variables
+    question_answer_from_context_prompt = PromptTemplate(
+        template=question_answer_prompt_template,
+        input_variables=["context", "question"],
+    )
+
+    # Create a chain by combining the prompt template and the language model
+    question_answer_from_context_cot_chain = question_answer_from_context_prompt | question_answer_from_context_llm.with_structured_output(
+        QuestionAnswerFromContext)
+    return question_answer_from_context_cot_chain
+
+
+def answer_question_from_context(question, context, question_answer_from_context_chain):
+    """
+    Answer a question using the given context by invoking a chain of reasoning.
+
+    Args:
+        question: The question to be answered.
+        context: The context to be used for answering the question.
+
+    Returns:
+        A dictionary containing the answer, context, and question.
+    """
+    input_data = {
+        "question": question,
+        "context": context
+    }
+    print("Answering the question from the retrieved context...")
+
+    output = question_answer_from_context_chain.invoke(input_data)
+    answer = output.answer_based_on_content
+    return {"answer": answer, "context": context, "question": question}
