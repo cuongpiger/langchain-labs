f46858ef471ba4ea6ce5a2b9c12760c8d7f5abe1
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-02-28 14:31:23 +0700

    [chore] vks with deepseek

--- tutorials/components/chat-models/modules/utils.py
+++ tutorials/components/chat-models/modules/utils.py
@@ -1,4 +1,5 @@
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
+from langchain.schema import Document
 
 def load_env_to_dict(file_path: str) -> Optional[Dict[str, Any]]:
     env_dict = {}
@@ -12,3 +13,15 @@ def load_env_to_dict(file_path: str) -> Optional[Dict[str, Any]]:
             key, value = line.split('=', 1)
             env_dict[key.strip()] = value.strip()
     return env_dict
+
+def print_documents(documents: List[Document]):
+    print("Amount of documents:", len(documents))
+    for doc_th, document in enumerate(documents):
+        print(f"Document {doc_th}:\n{document.page_content}", end="\n\n"+ 100*"-")
+
+def from_documents_to_context(documents: List[Document]) -> str:
+    context = ""
+    for doc_th, document in enumerate(documents):
+        context = context + f"- Document {doc_th + 1}:\n{document.page_content}\n\n"
+
+    return context
