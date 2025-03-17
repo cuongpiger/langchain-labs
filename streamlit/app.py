512da6b0b5a11abc9eaccd802a3a731aec37ee70
Cuong. Duong Manh <cuongpigerr@gmail.com>
2025-03-17 11:37:10 +0700

    [docs] update README docs

---
+++ streamlit/app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+from ensemble import ensemble_retriever_from_docs
+from full_chain import create_full_chain, ask_question
+from local_loader import load_txt_files
+
+st.set_page_config(page_title="LangChain & Streamlit RAG")
+st.title("LangChain & Streamlit RAG")
+
+
+def show_ui(qa, prompt_to_user="How may I help you?"):
+    if "messages" not in st.session_state.keys():
+        st.session_state.messages = [{"role": "assistant", "content": prompt_to_user}]
+
+    # Display chat messages
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    # User-provided prompt
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        with st.chat_message("user"):
+            st.write(prompt)
+
+    # Generate a new response if last message is not from assistant
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = ask_question(qa, prompt)
+                st.markdown(response.content)
+        message = {"role": "assistant", "content": response.content}
+        st.session_state.messages.append(message)
+
+
+@st.cache_resource
+def get_retriever(openai_api_key=None):
+    docs = load_txt_files()
+    embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/paraphrase-multilingual-mpnet-base-v2",
+    )
+    return ensemble_retriever_from_docs(docs, embeddings=embeddings)
+
+
+def get_chain(openai_api_key=None, huggingfacehub_api_token=None):
+    ensemble_retriever = get_retriever(openai_api_key=openai_api_key)
+    chain = create_full_chain(ensemble_retriever,
+                              openai_api_key=openai_api_key,
+                              chat_memory=StreamlitChatMessageHistory(key="langchain_messages"))
+    return chain
+
+
+def get_secret_or_input(secret_key, secret_name, info_link=None):
+    if secret_key in st.secrets:
+        st.write("Found %s secret" % secret_key)
+        secret_value = st.secrets[secret_key]
+    else:
+        st.write(f"Please provide your {secret_name}")
+        secret_value = st.text_input(secret_name, key=f"input_{secret_key}", type="password")
+        if secret_value:
+            st.session_state[secret_key] = secret_value
+        if info_link:
+            st.markdown(f"[Get an {secret_name}]({info_link})")
+    return secret_value
+
+
+def run():
+    ready = True
+
+    if ready:
+        chain = get_chain(openai_api_key="", huggingfacehub_api_token="")
+        st.subheader("Ask me questions about this week's meal plan")
+        show_ui(chain, "What would you like to know?")
+    else:
+        st.stop()
+
+
+run()
